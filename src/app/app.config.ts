b12c5cce6a2bad3ed4f08c0782b3812487ddcdd2
prashant <prashant2231109@akgec.ac.in>
2026-03-31 16:05:25 +0530

    removed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { authInterceptor } from './features/auth/auth.interceptor';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    ),
+    provideHttpClient(),
     provideRouter(routes)
   
     
